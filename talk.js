c95e588880162176b57ad5e53049d2461fd4eb42
HJW-XZY <hithejunwei@gmail.com>
2024-05-09 16:21:36 +0800

    Your commit message

--- talk.js
+++ talk.js
@@ -1,6 +1,6 @@
 $(function() {
     $('#yes').click(function(event) {
-        modal('我就知道小姐姐您一定会愿意的。(^_^)', function() {
+        modal('我就知道你也喜歡我哦！(^_^)', function() {
             $('.page_one').addClass('hide');
             $('.page_two').removeClass('hide');
             // typeWrite();
@@ -9,48 +9,48 @@ $(function() {
         });
     });
     $('#no').click(function(event) {
-        modal('明人不说暗话！', A);
+        modal('點錯啦，重來！', A);
     });
 });
 
 function A() {
-    modal('我喜欢你！', B);
+    modal('我喜歡你！', B);
 }
 
 function B() {
-    modal('我知道你在等我这一句话', C);
+    modal('薛紫玥同學', C);
 }
 
 function C() {
-    modal('请您不要拒绝我', D);
+    modal('或許以後我們會更換稱謂', D);
 }
 
 function D() {
-    modal('拒绝我，不存在的', E);
+    modal('但是請相信', E);
 }
 
 function E() {
-    modal('这辈子都不可能让你离开我', F);
+    modal('我會像熱戀時一樣喜歡你', F);
 }
 
 function F() {
-    modal('跟我走吧', G);
+    modal('把你當做我的唯一', G);
 }
 
 function G() {
-    modal('房产证上写你名', H);
+    modal('相信我們兩個人互相支持', H);
 }
 
 function H() {
-    modal('我会做饭', I);
+    modal('會比之前的我們更強大~', I);
 }
 
 function I() {
-    modal('爱你。么么哒！', J)
+    modal('也謝謝你喜歡我', J)
 }
 
 function J() {
-    modal('行，我们去民政局登记吧', function() {
+    modal('遇見你 是我今年最幸運的事 相信也會是這輩子最幸運的事~', function() {
         fireworks();
     });
 }
